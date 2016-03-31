a7756fdaf74841301f1f36e231223316dc175abb
Kyle Much <kylemuch@gmail.com>
2016-03-31 12:21:43 -0700

    second commit

--- routes/albums.js
+++ routes/albums.js
@@ -8,17 +8,37 @@ function albums() {
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  albums().select().then(function (records) {
+    res.render('index', {allAlbums: records});
+  });
 });
 
 router.get('/new', function(req, res, next){
   res.render('new');
 });
 
-router.post('/', function(req, res, next) { console.log(req.body)
-  albums().insert({ name: req.body.album_name, artist:req.body.artist_name, genre:req.body.genre, stars:req.body.stars, explicit:req.body.explicit }).then(function () {
+router.get('/:id', function(req, res, next){
+  albums().where({id: req.params.id}).first().then(function(record){
+    res.render('show', {theAlbum: record});
+  });
+});
+
+router.get('/:id/edit', function(req, res, next){
+  albums().where({id: req.params.id}).first().then(function(record){
+    res.render('edit', {theAlbum: record});
+  });
+});
+
+router.post('/', function(req, res, next) {
+  var explicit = req.body.explicit;
+  if(!explicit){
+    explicit = false;
+  }
+  albums().insert({ name: req.body.album_name, artist:req.body.artist_name, genre:req.body.genre, stars:req.body.stars, explicit: explicit }).then(function () {
     res.redirect('/albums');
   });
 });
+
+
 
 module.exports = router;
